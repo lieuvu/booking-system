90dff21307d6d4445b0c54c3475d9482eea31981
Lieu Vu <dinhlieuvu@gmail.com>
2020-05-04 21:12:49 +0300

    Add more test

--- test/test-ci.test.ts
+++ test/test-ci.test.ts
@@ -2,5 +2,10 @@ import { filterByTerm } from '../src/test-ci';
 
 test ('Should filter a corrrect term', () => {
     const result = filterByTerm([{ url: 'string1' }, { url: 'string2' }, { url: 'java1' }], 'java');
-    expect(result).toEqual([{url: 'java1'}])
+    expect(result).toEqual([{url: 'java1'}]);
+})
+
+test ('Should return empty array if no search tearm found', () => {
+    const result = filterByTerm([{ url: 'string1' }, { url: 'string2' }, { url: 'string3' }], 'java');
+    expect(result).toEqual([]);
 })
